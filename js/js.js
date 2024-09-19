9777aa30a6f75f46b23d48151737d0ecd7642873
Esmot-ara-lajuk <esmotaralajuk278@gmail.com>
2024-09-19 21:08:41 +0600

    first

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ document.getElementById('button-login').addEventListener('click', function(event
     const pinNumber = document.getElementById('pin-number').value;
      
     if(phoneNumber === '1234' && pinNumber === '0000'){
-        window.location.href = '/home.html'
+        window.location.href = '/modeul-27/home.html'
     }
     else{
         alert('False Information');
